8333e1b5b42b0527d3a71cca21551ca56ea50fbb
Jitse Swart <jitseswart059@gmail.com>
2025-06-11 15:32:36 +0200

    databaseremade

---
+++ database/migrations/2025_06_11_131625_create_answers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('answer');
+            $table->foreignId('questionID')->constrained('questions');
+            $table->foreignId('studentID')->constrained('student');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
