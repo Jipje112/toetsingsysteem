438292affc5127414b0729ef5dbc5906db27732f
Jitse Swart <jitseswart059@gmail.com>
2025-06-17 10:57:04 +0200

    v0.17 broken still

--- resources/views/components/databasetestconnection.blade.php
+++ resources/views/components/databasetestconnection.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Users List</title>
-</head>
-<body>
-    <h1>Users:</h1>
-    <ul>
-        @foreach ($users as $user)
-            <li>{{ $user->name }} ({{ $user->email }})</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+<x-databasetestconnection :users="$users" />
+@props(['users'])
+
+<ul>
+    @foreach ($users as $user)
+        <li>{{ $user->name }} ({{ $user->email }})</li>
+    @endforeach
+</ul>
